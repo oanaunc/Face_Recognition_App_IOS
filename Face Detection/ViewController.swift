fe7194bde891cb01098242266c70111009727c0c
Oana Unciuleanu <oanaunciuleanu@gmail.com>
2020-04-16 15:16:38 +0300

    Initial Commit

---
+++ Face Detection/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Face Detection
+//
+//  Created by Oana on 16/04/2020.
+//  Copyright © 2020 Oana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
